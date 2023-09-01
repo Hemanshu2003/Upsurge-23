cec9766b50efbb43581c61634e53fecfd268a280
Hemanshu2003 <113788603+Hemanshu2003@users.noreply.github.com>
2023-09-01 07:07:59 +0530

    gltf1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import gltf from 'rollup-plugin-gltf';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    gltf({
+      include: '**/*.gltf',
+      exclude: 'artwork/*.gltf',
+      inline: false,
+    }),
+  ],
+});
