40b40992c24f509f849cc20fdb116fee8a7a3f04
Hemanshu2003 <113788603+Hemanshu2003@users.noreply.github.com>
2023-09-10 16:59:52 +0530

    color

--- src/HomePage/SmakathonComponent/SmakathonComponent.jsx
+++ src/HomePage/SmakathonComponent/SmakathonComponent.jsx
@@ -20,23 +20,12 @@ export default function SmakathonComponent() {
           <p>
             Lorem ipsum dolor sit, amet conse ctetur adipi sicing elit. Aliquid,
             beatae alias esse dicta tenetur repell endus ducimus dolor emque et
-            quos lauda ntium aliquam dolore! Animi accus antium totam simi lique
-            quam corrupti. Volupt atum, dolorem?
+            quos lauda ntium aliquam dolore!
           </p>
-          <button className="reg_btn">
-            P L A Y
-            <div id="clip">
-              <div id="leftTop" className="corner"></div>
-              <div id="rightBottom" className="corner"></div>
-              <div id="rightTop" className="corner"></div>
-              <div id="leftBottom" className="corner"></div>
-            </div>
-            <span id="rightArrow" className="arrow"></span>
-            <span id="leftArrow" className="arrow"></span>
+
+          <button className="regBtn">
+            <span>register now!</span>
           </button>
-        </div>
-        <div className="curtains">
-          <div className="overlay"></div>
         </div>
       </div>
     </div>
