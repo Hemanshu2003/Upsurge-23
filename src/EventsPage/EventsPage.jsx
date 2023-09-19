751f595b6cf06c01e616ccf78d807a2ea39180a0
Hemanshu2003 <113788603+Hemanshu2003@users.noreply.github.com>
2023-09-19 13:15:01 +0530

    sd

--- src/EventsPage/EventsPage.jsx
+++ src/EventsPage/EventsPage.jsx
@@ -1,4 +1,10 @@
 import './EventsPage.css';
 export default function EventsPage() {
-  return <eventspage></eventspage>;
+  return (
+    <eventspage>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Hello this is event Page
+      </h1>
+    </eventspage>
+  );
 }
